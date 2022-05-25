d5949604a9b1a982c468a20917a2bdfed2521a49
Beth <bethmelmtv@gmail.com>
2022-05-24 23:43:50 -0700

    worked on app.js file

--- src/App.js
+++ src/App.js
@@ -1,39 +1,54 @@
 // import logo from './logo.svg';
 import './App.css';
-import { useState, useEffefct } from 'react';
-import { getUser } from './services/fetch-utils';
-import { Browser as Router, Switch, NavLink, Route, Redirect } from 'react-router-dom';
+import { useState } from 'react';
+import { logout } from './services/fetch-utils';
+import { Browser as Router, Switch, Link, Route, Redirect } from 'react-router-dom';
 import CreatePage from './CreatePage';
 import AuthPage from './AuthPage';
 import ListPage from './ListPage';
 import UpdatePage from './UpdatePage';
 import './App.css';
-import { logout } from './services/fetch-utils';
 
 export default function App() {
-  //make debit card... (states) so children can pass info back to parents via a callback/debit card
+  //make debit card (states) so children can pass info back to parents via a callback/debit card
+  //tracks user state, passing the setUser callback to the AuthPage
+
+  const [currentUser, setCurrentUser] = useState(localStorage.getItem('supabase.auth.token'));
+  //Use a ternary to decide whether to let users visit particular routes, depending on whether there
+  //is a user in App.js state
 
   return (
-    <>
-      <Switch>
-        <Route exact path="/">
-          <AuthPage />
-        </Route>
-
-        <Route exact path="">
-          <CreatePage />
-        </Route>
-
-        <Route exact path="">
-          <ListPage />
-        </Route>
-
-        <Route exact path="">
-          <UpdatePage />
-        </Route>
-      </Switch>
-    </>
+    //If user is logged in, header contains logout button and links to the Create and List pages.
+    <Router>
+      {currentUser && (
+        <nav>
+          <button> Logout</button>
+          <Link to="/create">Create</Link>
+          <Link to="/list">List</Link>
+          <button onClick={logout}>LogOut</button>
+        </nav>
+      )}
+      <div>
+        <Switch>
+          <Route exact path="/">
+            {!currentUser ? (
+              <AuthPage to="/" setCurrentUser={setCurrentUser} />
+            ) : (
+              <Redirect to="/items" />
+            )}
+          </Route>
+          <Route exact path="/items">
+            {!currentUser ? <Redirect to="/" /> : <ListPage />}
+          </Route>
+          <Route exact path="/create">
+            {!currentUser ? <Redirect to="/" /> : <CreatePage />}
+          </Route>
+          <Route exact path="/items/:id">
+            {!currentUser ? <Redirect to="/" /> : <UpdatePage />}
+          </Route>
+          `
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-// export default App;
